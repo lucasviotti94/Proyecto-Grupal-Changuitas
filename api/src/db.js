71a89d4c1affa790f4651173f655f50c4ff04e67
lucasviotti94 <lucasviotti@iresm.edu.ar>
2022-10-18 17:55:04 -0300

    Un commit descente

--- api/src/db.js
+++ api/src/db.js
@@ -2,15 +2,21 @@ require("dotenv").config();
 const { Sequelize, Op } = require("sequelize");
 const fs = require("fs");
 const path = require("path");
-const { DB_USER, DB_PASSWORD, DB_HOST } = process.env;
+const {
+  DB_USER,
+  DB_PASSWORD,
+  DB_HOST,
+  PGHOST,
+  PGPASSWORD,
+  PGUSER,
+  PGDATABASE,
+  DATABASE_URL,
+} = process.env;
 
-const sequelize = new Sequelize(
-  `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/jobplatform`,
-  {
-    logging: false, // set to console.log to see the raw SQL queries
-    native: false, // lets Sequelize know we can use pg-native for ~30% more speed
-  }
-);
+const sequelize = new Sequelize(DATABASE_URL, {
+  logging: false, // set to console.log to see the raw SQL queries
+  native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+});
 const basename = path.basename(__filename);
 
 const modelDefiners = [];
@@ -38,7 +44,8 @@ sequelize.models = Object.fromEntries(capsEntries);
 
 // En sequelize.models están todos los modelos importados, PopUp como propiedades
 // Para relacionarlos hacemos un destructuring
-const { Admin, Chat, Message, Contract, Job, User, Worker, Country, PopUp } = sequelize.models;
+const { Admin, Chat, Message, Contract, Job, User, Worker, Country, PopUp } =
+  sequelize.models;
 
 // Aca vendrian las relaciones
 // Product.hasMany(Reviews);
@@ -61,43 +68,38 @@ Worker.belongsTo(User);
 Job.belongsToMany(Worker, { through: "Works_Jobs" });
 Worker.belongsToMany(Job, { through: "Works_Jobs" });
 
-
 User.belongsToMany(Worker, { as: "Favorites", through: "Fav" });
 Worker.belongsToMany(User, { as: "Favorites", through: "Fav" });
 
 // User.hasMany(PopUp)
 
-
-User.hasMany(PopUp, {as: "Emiter"})
-PopUp.belongsTo(User,{as:"Emiter"})
-
+User.hasMany(PopUp, { as: "Emiter" });
+PopUp.belongsTo(User, { as: "Emiter" });
 
 // User.hasMany(PopUp,{as:"Receiver"})
-User.hasMany(PopUp, {as: "Receiver"})
-PopUp.belongsTo(User,{as:"Receiver"})
+User.hasMany(PopUp, { as: "Receiver" });
+PopUp.belongsTo(User, { as: "Receiver" });
 
-User.hasMany(Chat, {as:"Host"});
-Chat.belongsTo(User, {as:"Host"});
+User.hasMany(Chat, { as: "Host" });
+Chat.belongsTo(User, { as: "Host" });
 
-User.hasMany(Chat, {as:"Guest"});
-Chat.belongsTo(User, {as:"Guest"});
+User.hasMany(Chat, { as: "Guest" });
+Chat.belongsTo(User, { as: "Guest" });
 /* 
 Worker.hasMany(Chat);
 Chat.belongsTo(Worker); */
 
-User.hasMany(Message, {as:"Emitter"});
-Message.belongsTo(User, {as:"Emitter"});
+User.hasMany(Message, { as: "Emitter" });
+Message.belongsTo(User, { as: "Emitter" });
 
-User.hasMany(Message, {as:"Recibidor"});
-Message.belongsTo(User, {as:"Recibidor"});
+User.hasMany(Message, { as: "Recibidor" });
+Message.belongsTo(User, { as: "Recibidor" });
 
 /* Worker.hasMany(Message);
 Message.belongsTo(Worker); */
 
 Chat.hasMany(Message);
-Message.belongsTo(Chat)
-
-
+Message.belongsTo(Chat);
 
 // let probando = async () => {
 //   const agregando = await Videogame.create({nombre:'juanchito',descripcion:'alto',rating: 3.2,plataformas: 'Playstation'})
@@ -108,4 +110,3 @@ module.exports = {
   conn: sequelize,
   Op, // para importart la conexión { conn } = require('./db.js');
 };
-
